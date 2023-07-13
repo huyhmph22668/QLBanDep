54a14e07abe539ed4e372cc6e20ab617e7b098b9
Bui Minh Hieu <101169185+BuiMinhHieu21306@users.noreply.github.com>
2023-07-13 22:02:23 +0700

    ViewNhanVien

---
+++ src/models/ChucVu.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Admin 88
+ */
+public class ChucVu {
+     String id, ten;
+
+    public ChucVu() {
+    }
+
+    public ChucVu(String id, String ten) {
+        this.id = id;
+        this.ten = ten;
+    }
+
+    public ChucVu(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    @Override
+    public String toString() {
+        return ten;
+    }
+
+    
+}
---
+++ src/repositorys/IChucVuRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package repositorys;
+
+import java.util.List;
+import models.ChucVu;
+
+/**
+ *
+ * @author Admin 88
+ */
+public interface IChucVuRepository {
+    public List<ChucVu> getAllChucVu();
+}
